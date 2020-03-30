7f3d4d190016bb4b779fba00197922c5dae5acaa
Jacob <jtmcschwartz@gmail.com>
2020-03-29 20:51:18 -0700

    Table with Button

---
+++ app.js
@@ -0,0 +1,20 @@
+// append row to the HTML table
+function appendRow() {
+    var tbl = document.getElementById('board'), // table reference
+        row = tbl.insertRow(tbl.rows.length),      // append table row
+        i;
+    // insert table cells to the new row
+    for (i = 0; i < tbl.rows[0].cells.length; i++) {
+        createCell(row.insertCell(i), i, 'row');
+    }
+}
+ 
+// create DIV element and append to the table cell
+function createCell(cell, text, style) {
+    var div = document.createElement('div'), // create DIV element
+        txt = document.createTextNode(text); // create text node
+    div.appendChild(txt);                    // append text node to the DIV
+    div.setAttribute('class', style);        // set DIV class attribute
+    div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+    cell.appendChild(div);                   // append DIV to the table cell
+}
