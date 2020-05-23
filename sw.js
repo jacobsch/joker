885596c7689cb45cc4f9bbac3773f56d3f703886
Jacob Schwartz <jtmcschwartz@gmail.com>
2020-05-22 17:20:22 -0700

    Added Offline Support Via Service Worker

---
+++ sw.js
@@ -0,0 +1,31 @@
+importScripts('/cache-polyfill.js');
+
+const version = "0.1";
+const cacheName = `jokerSW-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/styles.css`,
+        `/app.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
