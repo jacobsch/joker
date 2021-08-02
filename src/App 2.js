c26148ec37e839773b752b32e3475ac765c5d4f0
Jacob Schwartz <jtmcschwartz@gmail.com>
2021-08-02 10:53:13 -0700

    Initial Commit - create-react-app

---
+++ src/App 2.js
@@ -0,0 +1,12 @@
+import Landing from './Landing'
+function App() {
+  return (
+      <Landing></Landing>
+  Welcome, how many players would like to play?
+      <button>-</button>
+      <div ref={'playerCount'}>#</div>
+  <button>+</button>
+  )
+}
+
+export default App;
